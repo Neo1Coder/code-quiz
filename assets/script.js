69f3eecf2020e6d860778df9c2936bd510c24aee
FlexFocus <mustafamire@gmail.com>
2021-06-27 18:39:03 +0100

    added variables for questions

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+        title: "Which of the following is true about variable naming conventions in JavaScript?:",
+        choices: ["You should not use any of the JavaScript reserved keyword as variable name.", "JavaScript variable names should not start with a numeral (0-9)", " Both of the above", " None of the above"],
+        answer: "Both of the above"
+    },
+    {
+        title: "Which of the following type of variable is visible everywhere in your JavaScript code?",
+        choices: ["global variable", "local variable", "Both of the above.", "None of the above"],
+        answer: "global variable"
+    },
+    {
+        title: "Which built-in method returns the character at the specified index?",
+        choices: ["characterAt()", "getCharAt()", "charAt()", "None of the above."],
+        answer: "charAt()"
+    },
+    {
+        title: "Which of the following function of Array object returns a new array comprised of this array joined with other array(s) and/or value(s)?.",
+        choices: ["concat()", "pop()", "push()", "some()"],
+        answer: "concat()"
+    },
+    {
+        title: "Which of the following function of Number object defines how many total digits to display of a number?",
+        choices: ["toExponential()", "toFixed()", " toLocaleString()", "toPrecision()"],
+        answer: "toPrecision()"
+    },
+
+];
